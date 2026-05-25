c27ddfda2b84099c69be456715427a01b2e58044
UltraPokeMaster01 <Ultrapokemaster001@gmail.com>
2026-05-25 15:32:46 +1000

    Add files via upload

--- settings.php
+++ settings.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,3 +10,16 @@
         $sql_db = "exhibition_db";
     ?>
 </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php
+        $host = "localhost";
+        $user = "root";
+        $pwd = "";
+        $sql_db = "exhibition_db";
+    ?>
+</body>
+>>>>>>> 218533e2f63097d7d2d44de904ee319ebbe64fe0
